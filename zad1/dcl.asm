d881694b0f1242de276aff492ab0e987a05c5dab
cezarybednarz <cezary.bednarz@gmail.com>
2020-03-28 20:35:35 +0100

    zad1: added two exit codes and 6 arrays in .bss

--- zad1/dcl.asm
+++ zad1/dcl.asm
@@ -1,22 +1,82 @@
-;; hello.asm
-;; Wypisuje określony ciąg 14 znaków
-;;
+SYS_WRITE equ 1
+SYS_EXIT  equ 60
+STDOUT    equ 1
+MAX_LINE  equ 80
 
-segment .data
-msg     db      "Hello World!", 0Ah    ; umieszcza w segmencie danych ciąg znaków zakończony znakiem końca linii
+N         equ 42          ; Liczba znaków, która można szyfrować
+NN        equ 1764        ; 42^2
+NNN       equ 74088       ; 42^3
 
-segment .text
-        global  _start
 
+; Wykonanie programu zaczyna się od etykiety _start.
+global _start
+
+
+
+
+section .data
+
+
+section .rodata
+  new_line db `\n`
+
+
+section .bss
+  Lperm   resb N
+  Rperm   resb N
+  Tperm   resb N
+  Linv    resb N
+  Rinv    resb N
+  Tinv    resb N
+
+
+section .text
 _start:
-        mov     eax, 4
-        mov     ebx, 1
-        mov     ecx, msg        ; adres pierwszego znaku do wyświetlenia
-        mov     edx, 14         ; liczba znaków do wyświetlenia
-        int     80h             ; wywołanie funkcji systemowej wyświetlającej ciąg znaków o danej długości
+  lea     rbp, [rsp + 8]  ; adres args[0]
+loop:
+  mov     rsi, [rbp]      ; adres kolejnego argumentu
+  test    rsi, rsi
+  jz      exit            ; Napotkano zerowy wskaźnik, nie ma więcej argumentów.
+  cld                     ; Zwiększaj indeks przy przeszukiwaniu napisu.
+  xor     al, al          ; Szukaj zera.
+  mov     ecx, MAX_LINE   ; Ogranicz przeszukiwanie do MAX_LINE znaków.
+  mov     rdi, rsi        ; Ustaw adres, od którego rozpocząć szukanie.
+  repne \
+  scasb                   ; Szukaj bajtu o wartości zero.
+  mov     rdx, rdi
+  mov     eax, SYS_WRITE
+  mov     edi, STDOUT
+  sub     rdx, rsi        ; liczba bajtów do wypisania
+  syscall
+  mov     eax, SYS_WRITE
+  mov     edi, STDOUT
+  mov     rsi, new_line   ; Wypisz znak nowej linii.
+  mov     edx, 1          ; Wypisz jeden bajt.
+  syscall
+  add     rbp, 8          ; Przejdź do następnego argumentu.
+  jmp     loop
+  
+  
+  
+exit_err:                   
+  mov     eax, SYS_EXIT
+  mov     edi, 1          ; kod powrotu 1
+  syscall
+  ret
+exit:                    
+  mov     eax, SYS_EXIT
+  xor     edi, edi        ; kod powrotu 0
+  syscall
 
-; wyjscie z programu
-        mov     eax, 1
-        xor     ebx, ebx
-        int     0x80
-; KONIEC PROGRAMU
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
--- zad1/dcl_easy.c
+++ zad1/dcl_easy.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
-const char CHARS = 42;
+#define CHARS 42
 
 bool sprawdz_i_odwroc(char* perm, char* inv) {
     if(strlen(perm) != CHARS) {
@@ -22,7 +23,6 @@ bool sprawdz_i_odwroc(char* perm, char* inv) {
         if(inv[perm[i]] != CHARS) {
             return 1;
         }
-        
         inv[perm[i]] = i;
     }
     return 0;
@@ -32,16 +32,26 @@ char modulo(char n, char diff) {
     return (n + diff + 42) % 42;
 }
 
+char perm[CHARS][CHARS][CHARS];
+
 int main(int argc, char* argv[]) {
     if(argc != 5) {
+        return 1;
+    }
+    if(argv[4][2]) {
         return 1;
     }
     char *L = argv[1];
     char *R = argv[2];
     char *T = argv[3];
     char Linv[CHARS], Rinv[CHARS];
-    char Lkey = argv[4][0] - '1';
-    char Rkey = argv[4][1] - '1';    
+    char Lkey = argv[4][0];
+    char Rkey = argv[4][1];    
+    if(Lkey < '1' || Lkey > 'Z' || Rkey < '1' || Rkey > 'Z') {
+        return 1;
+    }
+    Rkey -= '1';
+    Lkey -= '1';
     if(sprawdz_i_odwroc(L, Linv) || sprawdz_i_odwroc(R, Rinv)) {
         return 1;
     }
@@ -49,48 +59,62 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     for(int i = 0; i < CHARS; i++) {
-        if(T[T[i] - '1'] - '1' != i) {
+        if(T[T[i] - '1'] - '1' != i || T[i] - '1' == i) {
             return 1;
         }
     }
     for(int i = 0; i < CHARS; i++) {
-        L[i] -= '1';
-        R[i] -= '1';
         T[i] -= '1';
     }
     char c;
-    while(scanf(" %c", &c)) {
-        if(c < '1' || c > 'Z') {
-            return 1;
+    for(char LL = 0; LL < CHARS; LL++) {
+        for(char RR = 0; RR < CHARS; RR++) {
+            for(char CC = 0; CC < CHARS; CC++) {
+                c = CC;
+                    
+                c = modulo(c, RR);
+                c = R[c];
+                c = modulo(c, CHARS - RR);
+
+                c = modulo(c, LL);
+                c = L[c];
+                c = modulo(c, CHARS - LL);
+
+                c = T[c];
+
+                c = modulo(c, LL);
+                c = Linv[c];
+                c = modulo(c, CHARS - LL);
+
+                c = modulo(c, RR);
+                c = Rinv[c];
+                c = modulo(c, CHARS - RR);
+
+                perm[LL][RR][CC] = c + '1';
+            }
         }
-        c -= '1';
-        Rkey = (Rkey + 1) % CHARS;
-        if(Rkey == ('R' - '1') ||
-           Rkey == ('L' - '1') ||
-           Rkey == ('T' - '1')) {
-            Lkey = (Lkey + 1) % CHARS;
+    }
+    
+    const int len = 4096;
+    char buff[len + 10];
+    int curr = 0;
+    
+    while(true) {
+        curr = read(0, buff, len);
+        if(!curr) {
+            break;
         }
-        // permutacje
-        // Qr-1R-1Qr Ql-1L-1Ql T Ql-1LQl Qr-1RQr
-        c = modulo(c, Rkey);
-        c = R[c];
-        c = modulo(c, CHARS - Rkey);
-        
-        c = modulo(c, Lkey);
-        c = L[c];
-        c = modulo(c, CHARS - Lkey);
-        
-        c = T[c];
-        
-        c = modulo(c, Lkey);
-        c = Linv[c];
-        c = modulo(c, CHARS - Lkey);
-        
-        c = modulo(c, Rkey);
-        c = Rinv[c];
-        c = modulo(c, CHARS - Rkey);
-        
-        printf("%c", c + '1');
+        for(int i = 0; i < curr; i++) {
+            c = buff[i];
+            c -= '1';
+            Rkey = (Rkey + 1) % CHARS;
+            if(Rkey == ('R' - '1') ||
+                Rkey == ('L' - '1') ||
+                Rkey == ('T' - '1')) {
+                Lkey = (Lkey + 1) % CHARS;
+            }
+            printf("%c", perm[Lkey][Rkey][c]);
+        }
     }
     return 0;
 }

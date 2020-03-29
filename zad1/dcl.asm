e56bc8baf464ba21a09d101c36b67ff37222f098
cezarybednarz <cezary.bednarz@gmail.com>
2020-03-29 04:11:09 +0200

    prawie dziala sprawdzanie permutacji

--- zad1/dcl.asm
+++ zad1/dcl.asm
@@ -2,11 +2,12 @@ SYS_WRITE equ 1
 SYS_EXIT  equ 60
 STDOUT    equ 1
 MAX_LINE  equ 80
+FIRST     equ 49
+LAST      equ 90
 
 N         equ 42          ; Liczba znaków, która można szyfrować
 NN        equ 1764        ; 42^2
 NNN       equ 74088       ; 42^3
-
 
 ; Wykonanie programu zaczyna się od etykiety _start.
 global _start
@@ -22,31 +23,56 @@ section .rodata
 
 
 section .bss
-  Lperm   resb N
-  Rperm   resb N
-  Tperm   resb N
-  Linv    resb N
-  Rinv    resb N
-  Tinv    resb N
-
+  Linv    resb N          ; odwrocona permutacja L
+  Rinv    resb N          ; odwrocona permutacja R
+  Tinv    resb N          ; odwrocona permutacja T
+  L       resb 1          ; bebenek L 
+  R       resb 1          ; bebenek R
 
 section .text
 
-read_L:
+; sprawdza permutacje z %rdi i odwraca ją w %rsi
+inverse: 
+  xor     r8, r8          ; r8 to zmienna długości słowa, którą zeruję
+loop1:   
+  cmp byte[rdi+r8], 0     ; sprawdz czy koniec slowa
+  jz      inverse1        ; koniec petli (wyjdz)
+  cmp byte[rdi+r8], FIRST ; porównaj z pierwszym znakiem alfabetu ('1')
+  jb      exit_err        ; wyjdz jesli zly znak
+  cmp byte[rdi+r8], LAST  ; porównaj z ostatnim znakiem alfabetu ('Z')
+  jg      exit_err        ; wyjdz jesli zly znak
+  sub byte[rdi+r8], FIRST ; przesun kod ascii do zera (żeby '1' mialo kod 0 itd.)
+  inc     r8              ; zwieksz licznik liter
+  jmp     loop1           ; powrót pętli loop1
+inverse1:
+  cmp     r8, N           ; sprawdz czy ma 42 litery
+  jne     exit_err        ; jesli nie ma, return 1
+  
+  
+  
+  
+  
+  
+  ret
 
-read_R:
-
-read_T:
-
+; początek programu
 _start:
-  mov     rax, [rsp]
+  mov     rax, [rsp]      ; adres argc
   cmp     rax, 5          ; sprawdz czy jest 5 argumentów
   jne     exit_err        ; jeśli nie to return 1
   
+  lea     r9,  [rsp + 16] ; adres args[0] w r9
+  lea     r10, [rsp + 24] ; adres args[1] w r10
+  lea     r11, [rsp + 32] ; adres args[2] w r11
   
+; sprawdzanie permutacji L
+  mov     rdi, [r9]       ; przekaz 1. argument do funkcji inverse
+  mov     rsi, Linv       ; przekaz 2. argument do funkcji inverse
+  call    inverse         ; sprawdzenie i odwrócenie permutacji L
+
+;  TODO dwie pozostałe permutacje
   
-  
-  
+
 exit:                    
   mov     eax, SYS_EXIT
   xor     edi, edi        ; kod powrotu 0
